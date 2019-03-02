59c0761926263bf4b7ba99bebe2d25d46bcded68
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-03-01 22:52:15 -0300

    Evoluating app

--- src/main/java/com/tdev/suggestiontrackapp/config/SpotifyApiConfig.java
+++
@@ -1,6 +0,0 @@
-package com.tdev.suggestiontrackapp.config;
-
-public class SpotifyApiConfig {
-
-	private String clientId = "ae583995a2e149fdb43cdc2dc6c4d1c8";
-}
--- src/main/java/com/tdev/suggestiontrackapp/service/SuggestionTrackService.java
+++ src/main/java/com/tdev/suggestiontrackapp/service/SuggestionTrackService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.tdev.suggestiontrackapp.config.SpotifyApiConfig;
 import com.tdev.suggestiontrackapp.enums.GenreEnum;
 import com.tdev.suggestiontrackapp.enums.TemperatureUnitEnum;
 import com.tdev.suggestiontrackapp.model.currentWeather.CurrentWeatherResponse;
